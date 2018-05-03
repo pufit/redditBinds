035d20d36832dd6ce958bb7fad7b7cf4c6c8488c
pufit <budanov01@bk.ru>
2018-05-03 21:01:49 +0300

    Bug fix

--- redditKeys.js
+++ redditKeys.js
@@ -11,19 +11,43 @@
 // @require https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
 // ==/UserScript==
 
-indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
-
-up = $('[data-click-id=upvote]')[0];
-down = $('[data-click-id=downvote]')[0];
+var up, down,
+  indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
 
-$('body').keydown(function(e){
-  if(e.keyCode === 38 && (indexOf.call(up.children[0].classList, 'cZdGOJ') >= 0 || indexOf.call(up.children[0].classList, 'kDEiCN') >= 0)){
+function main(){
+  var wind = $('.exxCWQ');
+  if (wind.length){
+    var b = wind[0].children[0].children[0].children[0].children[0].children[0].children;
+    up = b[0];
+    down = b[2];
+  } else {
+    up = $('[data-click-id=upvote]')[0];
+    down = $('[data-click-id=downvote]')[0];
+  }
+  
+  $('body').bind('keydown', handler)
+}
+
+function handler(e){
+  if (e.keyCode === 38 && (indexOf.call(up.children[0].classList, 'cZdGOJ') >= 0 || indexOf.call(up.children[0].classList, 'kDEiCN') >= 0)){
     up.click();
     return false
   }
-  if(e.keyCode === 40 && (indexOf.call(down.children[0].classList, 'gquANw') >= 0 || indexOf.call(down.children[0].classList, 'kQgIul') >= 0)){
+  if (e.keyCode === 40 && (indexOf.call(down.children[0].classList, 'gquANw') >= 0 || indexOf.call(down.children[0].classList, 'kQgIul') >= 0)){
     down.click();
     return false
   }
-})
+}
+
+main()
+
+var loc = location.href;
+setInterval(function(){
+  if (location.href !== loc){
+    loc = location.href;
+    $('body').unbind('keydown', handler)
+    main();
+    console.log(loc)
+  }
+}, 100);
